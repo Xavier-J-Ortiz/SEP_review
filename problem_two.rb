7f0f71676189516ffe8eb17a574d4003a0b3554f
xortiz <xavier.ortiz.ch@gmail.com>
2017-11-02 20:51:22 -0400

    refactored a tad. I think it's passable

--- problem_two.rb
+++ problem_two.rb
@@ -1,20 +1,18 @@
 # Integers: Recreation One
 # had to use this rationale found here https://www.math.upenn.edu/~deturck/m170/wk3/lecture/sumdiv.html
 # and here https://www.math.upenn.edu/~deturck/m170/wk3/lecture/sumdiv.html
-
 require 'prime'
 
 def list_squared(m, n)
 	answer = [] 
 	(m..n).each do |location|
-		prime_factor = get_prime_factors(location)
 		sum_of_squares = 1
-		prime_factor.each do |key, value|
-			sum_of_prime_factor = 0
+		get_prime_factors(location).each do |key, value|
+			sum_of_exponent_series = 0
 			(0..value).each do |exponent|
-				sum_of_prime_factor += (key ** exponent) ** 2
+				sum_of_exponent_series += (key ** exponent) ** 2
 			end
-			sum_of_squares *= sum_of_prime_factor
+			sum_of_squares *= sum_of_exponent_series
 		end
 		answer.push([location, sum_of_squares]) if (sum_of_squares ** 0.5) % 1 == 0
 	end
@@ -22,23 +20,24 @@ def list_squared(m, n)
 end
 
 def get_prime_factors(location)
-	highest_prime_factor = (location ** 0.5) /2
-	list_of_primes = Prime.take_while { |i| i <= highest_prime_factor }
+	highest_prime_factor = (location ** 0.5) / 2
+	primes = Prime.take_while { |i| i <= highest_prime_factor }
+	check_for_prime_factors(primes, location)
+end
+
+def check_for_prime_factors(primes, location)
 	i = 0
-	number = location
 	prime_factors = {}
-	while i < list_of_primes.length
-		if number % list_of_primes[i] == 0
-			if prime_factors[list_of_primes[i]]
-				prime_factors[list_of_primes[i]] += 1
-			else
-				prime_factors[list_of_primes[i]] = 1
-			end
-			number /= list_of_primes[i]
+	while i < primes.length
+		if location % primes[i] == 0
+			prime_factors[primes[i]] = prime_factors[primes[i]] ? prime_factors[primes[i]] + 1 : 1
+			location /= primes[i]
 		else
 			i += 1
 		end
 	end
-	prime_factors[number] = 1 unless number == 1
+	prime_factors[location] = 1 unless location == 1
 	prime_factors
 end
+
+puts list_squared(1, 250).to_s
